9062fa01023524c2658ddf8b24f6786c72508127
aniljos <anil.jos@gmail.com>
2026-03-25 16:47:23 +0530

    Day 3 Session 3

---
+++ share/gadgetsReducer.ts
@@ -0,0 +1,31 @@
+import { CartItem } from "@/model/CartItem";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export type GadgetState = {
+    cart: CartItem[],
+    
+}
+const initialState: GadgetState = {
+    cart: [],
+    
+}
+
+//action: {type: "addtocart", payload: CartItem}
+//action: {type: "removeitem", id: 1}
+// export const gadgetsReducer = (state=initialState, action) => {
+
+//     if(action.type === "addtocart" && action.payload){
+
+//         //state.cart.push(action.payload);
+//         const cart = [...state.cart];
+//         cart.push(action.payload);
+//         return {
+//             ...state,
+//             cart: cart,
+//         }
+//     }
+//     return state;
+// }
+
+
+
